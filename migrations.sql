d411919cb211b889bd2035d448010cb2f3b1ac9c
yuxu060324 <yu.y.200017@gmail.com>
2025-12-20 20:03:48 +0900

    delete sql

--- migrations.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS latest_image (
-  id INTEGER PRIMARY KEY,
-  url TEXT NOT NULL
-);
-
-INSERT INTO latest_image (id, url)
-VALUES (1, 'https://example.com/initial.jpg')
-ON CONFLICT (id) DO NOTHING;
