004b6284ace516a51523d3496eee8124b3bf2062
yuxu060324 <yu.y.200017@gmail.com>
2025-12-27 06:36:24 +0900

    delete unused define

--- image_common.py
+++ image_common.py
@@ -1,6 +1,5 @@
 import os
 import datetime
-# from common_global import *
 
 try:
 	import urlparse
@@ -8,52 +7,14 @@ except ImportError:
 	import urllib.parse as urlparse
 
 HOME_ABSPATH = os.path.dirname(os.path.abspath(__file__))
-GITHUB_PROJECT_CONTENT_PATH = "https://raw.githubusercontent.com/yuxu060324/FlexMessage/master/"
-RENDER_PROJECT_URL = "https://lineplanbotyamanaka.onrender.com/"
-
-DAY_OF_WEEK_LIST = ["月", "火", "水", "木", "金", "土", "日"]
-
-message_template_folder_name = "TemplateMessage"
 
 icon_folder_name = "images/"
-event_icon_folder_name = "event/"
 weather_icon_folder_name = "weather/"
 output_folder_name = "out/"
 
 # 天候コードをまとめているJSONファイル名
 weather_code_list_file_name = "weatherCodeList.json"
 
-# -------------------------------
-# jsonControl.py から参照する変数
-# -------------------------------
-
-# URL用のURL
-GITHUB_PROJECT_CONTENT_PATH_IMAGE = urlparse.urljoin(GITHUB_PROJECT_CONTENT_PATH, icon_folder_name)
-RENDER_PROJECT_PATH_IMAGE = urlparse.urljoin(RENDER_PROJECT_URL, output_folder_name)
-
-# 予定のアイコンを格納しているフォルダ
-ICON_EVENT_FOLDER_URL = urlparse.urljoin(GITHUB_PROJECT_CONTENT_PATH_IMAGE, event_icon_folder_name)
-# 作成したアイコンを格納しているフォルダ
-ICON_OUTPUT_FOLDER_URL = urlparse.urljoin(GITHUB_PROJECT_CONTENT_PATH_IMAGE, output_folder_name)
-
-# -------------------------------
-# Header
-# -------------------------------
-
-# "DATE"
-HEADER_TEXT_DATE = "DATE"
-HEADER_TEXT_DATE_COLOR = "#ffffffB0"
-HEADER_TEXT_DATE_SIZE = "sm"
-
-# text of date
-HEADER_TEXT_COLOR = "#c00000"
-HEADER_TEXT_SIZE = "xl"
-
-HEADER_BACK_COLOR = "#0367D3"
-
-# -------------------------------
-# Hero
-# -------------------------------
 
 OUTPUT_FILE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "static", "image", "out")
 FONT_FILE_PATH_MEIRYO = os.path.join(HOME_ABSPATH, "Font", "meiryo.ttc")
@@ -110,55 +71,10 @@ HERO_POSITION_TEMPERATURE = (WEATHER_FORECAST_MAP_SIZE[0], 0)
 HERO_POSITION_DETAIL_WEATHER = (0, WEATHER_FORECAST_MAP_SIZE[1])
 
 # ファイル名
-out_file_name_detail_weather = "detail_weather"
-out_file_name_temperature = "temperature_icon"
-out_file_name_weather_map = "weather_forecast_map"
 out_file_name_hero = "out_weather_"
 
 # 最高/最低気温の背景/枠 の色
-TEMPERATURE_MAX_BG_COLOR = "#ffc0c0"    # 最高気温の背景色
-TEMPERATURE_MAX_FG_COLOR = "#ff0000"    # 最高気温の枠/文字の色
-TEMPERATURE_MIN_BG_COLOR = "#c0c0ff"    # 最低気温の背景色
-TEMPERATURE_MIN_FG_COLOR = "#0000ff"    # 最低気温の枠/文字の色
-
-# heroファイル操作用
-OUT_FILE_PATH_DETAIL_WEATHER = os.path.join(OUT_FOLDER_PATH, out_file_name_detail_weather + ".png")
-OUT_FILE_PATH_WEATHER_MAP = os.path.join(OUT_FOLDER_PATH, out_file_name_weather_map + ".png")
-OUT_FILE_PATH_TEMPERATURE = os.path.join(OUT_FOLDER_PATH, out_file_name_temperature + ".png")
-
-# -------------------------------
-# Body
-# -------------------------------
-
-# イベントアイコンのファイルパス
-ICON_EVENT_FILE = {
-	"task": urlparse.urljoin(ICON_EVENT_FOLDER_URL, "task.png"),
-	"event": urlparse.urljoin(ICON_EVENT_FOLDER_URL, "event.png"),
-	"game": urlparse.urljoin(ICON_EVENT_FOLDER_URL, "game.png"),
-	"commu": urlparse.urljoin(ICON_EVENT_FOLDER_URL, "commu.png"),
-	"eating": urlparse.urljoin(ICON_EVENT_FOLDER_URL, "eating.png"),
-	"hospital": urlparse.urljoin(ICON_EVENT_FOLDER_URL, "hospital.png"),
-	"other": urlparse.urljoin(ICON_EVENT_FOLDER_URL, "other.png"),
-}
-
-EVENT_KIND = {
-	"1": "eating",
-	"2": "other",
-	"3": "commu",
-	"4": "other",
-	"5": "game",
-	"6": "event",
-	"7": "other",
-	"8": "other",
-	"9": "hospital",
-	"10": "other",
-	"11": "task",
-	"-": "other"
-}
-
-# -------------------------------
-# Footer
-# -------------------------------
-
-FOOTER_URL = "https://www.google.com/"
-GOOGLE_CALENDAR_URL = 'https://calendar.google.com/calendar/u/0/r'
+TEMPERATURE_MAX_BG_COLOR = "#ffc0c0"		# 最高気温の背景色
+TEMPERATURE_MAX_FG_COLOR = "#ff0000"		# 最高気温の枠/文字の色
+TEMPERATURE_MIN_BG_COLOR = "#c0c0ff"		# 最低気温の背景色
+TEMPERATURE_MIN_FG_COLOR = "#0000ff"		# 最低気温の枠/文字の色
